576b5c2e92e0a08cdac058c026ca7c3f717d44e6
Ndivhuwo Mbaimbai <ndivhuwo.mbaimbai@redpandasoftware.co.za>
2022-08-26 09:18:29 +0200

    Added followers implementation

---
+++ Tweet_Feed/Models/Follower.cs
@@ -0,0 +1,18 @@
+﻿namespace Tweet_Feed
+{
+    // Concrete Observers react to the updates issued by the Subject they had
+    // been attached to.
+    public class Follower : IObserver
+    { 
+        public Follower(string followerName)
+        {
+            FollowerName = followerName;
+        }
+        public string FollowerName { get; set; }
+
+        public void Update(IUser subject, string feed)
+        {  
+            Console.WriteLine($"{FollowerName} {feed}");
+        } 
+    }
+}
