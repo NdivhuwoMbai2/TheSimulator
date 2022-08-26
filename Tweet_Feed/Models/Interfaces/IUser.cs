76482a5a38966c19406f27529809631709d76d02
Ndivhuwo Mbaimbai <ndivhuwo.mbaimbai@redpandasoftware.co.za>
2022-08-26 09:17:06 +0200

    Iuser interface added..

---
+++ Tweet_Feed/Models/Interfaces/IUser.cs
@@ -0,0 +1,12 @@
+﻿namespace Tweet_Feed
+{
+    public interface IUser
+    { 
+        void Subscribe(IObserver observer); 
+        /// <summary>
+        /// receives feeds
+        /// </summary>
+        /// <param name="feed"></param>
+        void Notify(string feed);
+    }
+}
