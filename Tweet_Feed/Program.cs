702bb10478f6f53b1a8ef3d898b3028856b53537
Ndivhuwo Mbaimbai <ndivhuwo.mbaimbai@redpandasoftware.co.za>
2022-08-26 09:20:36 +0200

    program files and a process added

---
+++ Tweet_Feed/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Wait for user
+using Tweet_Feed;
+Console.WriteLine("TWEET FEED");
+Console.WriteLine("==========");
+Console.WriteLine("START"); 
+Processor processor = new Processor();
+Console.WriteLine("END");
+Console.ReadLine();
