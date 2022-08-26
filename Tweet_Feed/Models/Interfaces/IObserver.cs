e02a81978ba9866a7f2d3cfa189c2cfe17a515fa
Ndivhuwo Mbaimbai <ndivhuwo.mbaimbai@redpandasoftware.co.za>
2022-08-26 09:14:55 +0200

    added i observer interface

---
+++ Tweet_Feed/Models/Interfaces/IObserver.cs
@@ -0,0 +1,13 @@
+﻿namespace Tweet_Feed
+{
+    public interface IObserver
+    { 
+        public string FollowerName { get; set; }
+        /// <summary>
+        /// receives the user and the feed to print 
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="feed"></param>
+        void Update(IUser subject,string feed);
+    }
+}
